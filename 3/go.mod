f18ea2b7c9e6e1d7ec6e278b11382babf09bdd0c
Rhyker Wells <a.rhykerw@gmail.com>
2022-01-19 22:00:55 +0000

    Math functions, 2!

---
+++ 3/go.mod
@@ -0,0 +1,3 @@
+module math-2
+
+go 1.17
---
+++ 3/math.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	if x > 6 {
+		fmt.Println("More than 6")
+	} else {
+
+		fmt.Println("Less than 6")
+	}
+}
